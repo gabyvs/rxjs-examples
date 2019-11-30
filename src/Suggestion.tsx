3f1532687e8e2b766d8cf76bf472e3685ed53eda
Gaby Vazquez <gabs.vz@gmail.com>
2019-11-29 20:53:26 -0800

    Requesting suggestions.

--- src/Suggestion.tsx
+++ src/Suggestion.tsx
@@ -6,18 +6,19 @@ export interface SuggestionProps {
 }
 
 export const Suggestion: React.FC<SuggestionProps> = (props: SuggestionProps) => {
+  console.log('rendering suggestion: ', props.index);
   return (
     <div className={styles.suggestion}>
       <div className={styles.picture}>
-        <img className={styles.image}/>
+        <img className={styles.image} alt='suggestion'/>
       </div>
       <div className={styles.data}>
         <span className={styles.name}>Erik Meijer</span>
         <span className={styles.at}>@headinthebox</span>
       </div>
       <div className={styles.links}>
-        <a href="#" target="_blank" className={styles.link}>Follow</a>
-        <a href="#" className={styles.link}>Discard</a>
+        <button className={styles.link}>Follow</button>
+        <button className={styles.link}>Discard</button>
       </div>
     </div>
   );
--- src/SuggestionBox.tsx
+++ src/SuggestionBox.tsx
@@ -1,9 +1,45 @@
-import React          from 'react';
-import { Suggestion } from './Suggestion';
-import styles         from './SuggestionBox.module.css';
+import React, { useEffect, useState } from 'react';
+import { fromFetch }                  from 'rxjs/fetch';
+import { of }                         from 'rxjs';
+import { switchMap, catchError }      from 'rxjs/operators';
+import { Suggestion }                 from './Suggestion';
+import styles                         from './SuggestionBox.module.css';
+
+const requestStream = of('https://api.github.com/users');
 
 export const SuggestionBox: React.FunctionComponent = () => {
-  const suggestions = [1,2,3].map((n) => {
+  console.log('rendering suggestionBox');
+  const [suggestions, setSuggestions] = useState([]);
+
+
+
+  useEffect(() => {
+    console.log('useEffect');
+    const subscription = requestStream.subscribe((requestUrl) => {
+      const responseStream = fromFetch(requestUrl).pipe(
+        switchMap(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            return of({ error: true, message: `Error ${response.status}` });
+          }
+        }),
+        catchError(err => {
+          // Network or other error, handle appropriately
+          console.error(err);
+          return of({ error: true, message: err.message })
+        })
+      );
+
+      responseStream.subscribe((response) => {
+        console.log(response);
+      });
+    });
+
+    return subscription.unsubscribe;
+  });
+
+  const suggestionElements = suggestions.map((n) => {
     return <Suggestion index={n} key={n} />
   });
 
@@ -14,7 +50,7 @@ export const SuggestionBox: React.FunctionComponent = () => {
         <button className={styles.refresh}>Refresh</button>
       </div>
       <div className={styles.suggestions}>
-        {suggestions}
+        {suggestionElements}
       </div>
     </div>
   );
